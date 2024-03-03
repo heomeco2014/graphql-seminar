6a023cf2b131d9b012d8b16ba156438859626a64
Lam Ha <lam.ha@devsamurai.com>
2024-03-04 00:54:42 +0700

    set up cors

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
+    allowedHeaders: ['x-apollo-operation-name', 'apollo-require-preflight'],
     origin: [
       'https://graphql-seminar.onrender.com',
       'https://studio.apollographql.com',
